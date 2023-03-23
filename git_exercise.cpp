7029a14fea0d747cae2bec0b4a8fcb5872cd84c1
jenger <leonid_rodionov32@mail.ru>
2023-03-23 19:35:14 +0500

    Создал строку char, дальше буду писать методы для нее

---
+++ git_exercise.cpp
@@ -0,0 +1,14 @@
+﻿// Шаги, выполняемые в этом файле будут отражены в Git
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char str[] = "    hi       my    name is    Leonid   ";
+    cout << str << endl;
+    system("pause");
+    return 0;
+}
+
+
