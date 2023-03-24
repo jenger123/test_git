5e885a9c63e8e87e302e15ac1f9e981c35c3bc86
jenger <leonid_rodionov32@mail.ru>
2023-03-24 19:27:45 +0500

    Создал функцию, убирающую лишние пробелы

--- git_exercise.cpp
+++ git_exercise.cpp
@@ -2,11 +2,53 @@
 
 #include <iostream>
 using namespace std;
+
+
+char*  DeleteSpaces(char InputStr[], int ArraySize)
+{
+    int ptr1 = 0, spaces = 0, ptr2 = ArraySize-2;
+    while (InputStr[ptr1] == ' ')
+    {
+        ptr1++;
+        spaces++;
+    }
+    while (InputStr[ptr2] == ' ')
+    {
+        ptr2--;
+        spaces++;
+    }
+    for (int i = ptr1; i < ptr2; i++)
+    {
+        if (InputStr[i] == ' ' && InputStr[i + 1] == ' ')
+            spaces++;
+    }
+    int OutputSize = ArraySize - spaces + 1;
+    char* OutputStr = new char[OutputSize];
+    OutputStr[ArraySize - spaces - 1] = '\0';
+    int it = 0;
+    for (int i = ptr1; i <= ptr2; i++)
+    {
+        if (InputStr[i] == ' ' && InputStr[i + 1] == ' ')
+            continue;
+
+        else
+        {
+            OutputStr[it] = InputStr[i];
+            it++;
+        }
+    }
+    return OutputStr;
+}
+
 int main()
 {
     setlocale(LC_ALL, "rus");
     char str[] = "    hi       my    name is    Leonid   ";
-    cout << str << endl;
+    auto SizeStr = sizeof(str) / sizeof(str[0]);
+    char* str1 = DeleteSpaces(str, SizeStr);
+    for (int i{}; str1[i] != '\0'; i++)
+        cout << str1[i];
+    delete[] str1;
     system("pause");
     return 0;
 }
